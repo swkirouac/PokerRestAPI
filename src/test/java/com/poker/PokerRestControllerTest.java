ce686e6cc609a4733bf6e852bc72700189d8f389
swkirouac <swkirouac@gmail.com>
2021-01-24 11:40:35 -0500

    Add missing test to get cards of a player

--- src/test/java/com/poker/PokerRestControllerTest.java
+++ src/test/java/com/poker/PokerRestControllerTest.java
@@ -149,6 +149,25 @@ public class PokerRestControllerTest {
     }
 
     @Test
+    public void testGetListOfCardOfAPlayer() {
+        // Given
+        restTemplate.exchange(createURLWithPort("/game/0/player/Bob"), HttpMethod.POST, null, String.class);
+        restTemplate.exchange(createURLWithPort("/game/0/player/Patrick"), HttpMethod.POST, null, String.class);
+
+        for (int i=0; i<8; i++) {
+            restTemplate.exchange(createURLWithPort("/game/0/player/Bob/dealCard"), HttpMethod.POST, null, String.class);
+            restTemplate.exchange(createURLWithPort("/game/0/player/Patrick/dealCard"), HttpMethod.POST, null, String.class);
+        }
+
+        // When
+        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/game/0/player/Patrick"), HttpMethod.GET, null, String.class);
+
+        // Then
+        Assertions.assertEquals("[\"2 Diamonds\",\"4 Diamonds\",\"6 Diamonds\",\"8 Diamonds\",\"10 Diamonds\",\"Queen Diamonds\",\"Ace Clubs\",\"3 Clubs\"]", response.getBody());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
     public void testGetCountOfUndealtCardsPerSuit() {
         // Given
         restTemplate.exchange(createURLWithPort("/game/0/player/Bob"), HttpMethod.POST, null, String.class);
